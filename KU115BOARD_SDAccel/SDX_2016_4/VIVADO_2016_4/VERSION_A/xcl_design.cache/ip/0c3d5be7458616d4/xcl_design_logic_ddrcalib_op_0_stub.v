4354ca1fca82040abf0c4e7dfd457c3c8190ac00
sakinder <sakinder@live.com>
2017-05-19 21:37:08 -0500

    dive5
    dive5

---
+++ KU115BOARD_SDAccel/SDX_2016_4/VIVADO_2016_4/VERSION_A/xcl_design.cache/ip/0c3d5be7458616d4/xcl_design_logic_ddrcalib_op_0_stub.v
@@ -0,0 +1,21 @@
+// Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2016.4_sdx (win64) Build 1806307 Thu Mar  9 15:24:31 MST 2017
+// Date        : Tue May 16 10:00:45 2017
+// Host        : SakinderLaptop1 running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub -rename_top decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix -prefix
+//               decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix_ xcl_design_logic_ddrcalib_op_0_stub.v
+// Design      : xcl_design_logic_ddrcalib_op_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xcku115-flvb2104-2-e
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "util_reduced_logic,Vivado 2016.4_sdx" *)
+module decalper_eb_ot_sdeen_pot_pi_dehcac_xnilix(Op1, Res)
+/* synthesis syn_black_box black_box_pad_pin="Op1[3:0],Res" */;
+  input [3:0]Op1;
+  output Res;
+endmodule
